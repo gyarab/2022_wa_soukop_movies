7e30ddf80713cbe32391de4030bf237ce3ceec3c
Petr Soukop <83216742+soukop-petr@users.noreply.github.com>
2023-04-10 23:50:53 +0200

    Add files via upload

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Movie, Director
 
-# Create your views here.
+def directors(request):
+    context = {
+        'logic': True,
+        'title': "Nejoblíbenější režiséři",
+        'directors': Director.objects.all()
+    }
+    print(context)
+    return render(request, 'directors.html', context)
+
+
+def homepage(request):
+    context = {
+        "movies": Movie.objects.all()
+    }
+
+    return render(request, 'main.html', context)
+
