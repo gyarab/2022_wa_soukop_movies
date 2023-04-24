0ce7983072af3adbcff1b777a27fcaef6edaa5d1
Petr Soukop <83216742+soukop-petr@users.noreply.github.com>
2023-04-24 19:23:37 +0200

    Add files via upload

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,20 +1,50 @@
 from django.shortcuts import render
-from .models import Movie, Director
+from .models import Movie, Director, Actor, Genre
 
 def directors(request):
     context = {
-        'logic': True,
-        'title': "Nejoblíbenější režiséři",
         'directors': Director.objects.all()
     }
     print(context)
     return render(request, 'directors.html', context)
 
+def director(request, id):
+    context = {
+        "director": Director.objects.get(id=id)
+    }
+    return render(request, 'director.html', context)
 
-def homepage(request):
+def movies(request):
     context = {
         "movies": Movie.objects.all()
     }
+    return render(request, 'movies.html', context)
 
-    return render(request, 'main.html', context)
+def movie(request, id):
+    context = {
+        "movie": Movie.objects.get(id=id)
+    }
+    return render(request, 'movie.html', context)
 
+def actors(request):
+    context = {
+        "actors": Actor.objects.all()
+    }
+    return render(request, 'actors.html', context)
+
+def actor(request, id):
+    context = {
+        "actor": Actor.objects.get(id=id)
+    }
+    return render(request, 'actor.html', context)
+
+def homepage(request):
+    context = {
+        # TODO use first 10 top rated
+        "movies": Movie.objects.all(),
+        "actors": Actor.objects.all(),
+        "directors": Director.objects.all(),
+        "genres": Genre.objects.all(),
+    }
+    return render(request, 'homepage.html', context)
+  
